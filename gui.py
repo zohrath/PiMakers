60c9b6a968ee7161caab35a95c0e3a51e1febdb4
fera0008 <felix.ragnarsson.0008@student.uu.se>
2017-05-03 09:29:27 +0200

    Fixed main file so that the test works correctly

--- gui.py
+++ gui.py
@@ -6,14 +6,20 @@ from PyQt5 import QtCore
 
 class Sessionwindow():
     def __init__(self):
-        self.window = QtWidgets.QMainWindow()
+        self.window = QtWidgets.QDialog()
         self.initUI()
 
     def initUI(self):
+
+        testbutton = QtWidgets.QPushButton("Test")
+        testbutton.setMinimumSize(500, 200)
+
+
         self.window.setFixedSize(200, 200)
-        self.window.central_widget = QtWidgets.QWidget()
-        self.window.setCentralWidget(self.window.central_widget)
-        self.window.show()
+
+        vbox = QtWidgets.QVBoxLayout()
+        vbox.addWidget(testbutton)
+        self.window.setLayout(vbox)
 
 
 
@@ -24,11 +30,12 @@ class Mainmenu():
         self.app = app
         self.initUI()
 
+
     def initUI(self):
 
         startButton = QtWidgets.QPushButton("Starta ny mätning")
         startButton.setMinimumSize(500, 200)
-        startButton.clicked.connect(Sessionwindow)
+        startButton.clicked.connect(self.switch)
 
         visualizeButton = QtWidgets.QPushButton("Visa mätningar")
         visualizeButton.setMinimumSize(500, 200)
@@ -47,14 +54,19 @@ class Mainmenu():
         hbox.addLayout(vbox)
         hbox.addStretch(1)
 
-        w = QtWidgets.QWidget()
-        w.setLayout(hbox)
-        w.showFullScreen()
-        sys.exit(self.app.exec_())
+        self.w = QtWidgets.QWidget()
+        self.w.setLayout(hbox)
+
+    def switch(self):
+        self.w.hide()
+        nd = Sessionwindow()
+        nd.window.show()
 
 
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    Mainmenu(app)
+    main = Mainmenu(app)
+    main.w.showFullScreen()
+    sys.exit(app.exec_())
