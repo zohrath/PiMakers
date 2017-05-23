f4cd036db4ac91d336e3b161940615af87616ee8
fera0008 <felix.ragnarsson.0008@student.uu.se>
2017-05-23 12:31:34 +0200

    test

--- Communication.py
+++ Communication.py
@@ -1,15 +1,15 @@
 import serial, time
 
-    ser = serial.Serial("/dev/ttyUSB0", 57600, timeout=1, xonxoff=1)
-    ser.write("*RST\r\n")
-    ser.write("*CLS\r\n")
+ser = serial.Serial("/dev/ttyUSB0", 57600, timeout=1, xonxoff=1)
+ser.write("*RST\r\n".encode('utf-8'))
+ser.write("*CLS\r\n".encode('utf-8'))
 
 
 
 def measure():
 
-    ser.write("CONF:VOLT:DC 10,0.00001,(@101:118\r\n")
-    ser.write("READ?\r\n")
+    ser.write("CONF:VOLT:DC 10,0.00001,(@101:118\r\n".encode('utf-8'))
+    ser.write("READ?\r\n".encode('utf-8'))
     InstrumentReturn = ser.readline()
     try:
         bbb = map(float, InstrumentReturn.split(","))
@@ -22,16 +22,16 @@ def measure():
     return bbb
 
 def temperature(channels):
-    ser.write("CONF:TEMP TC, T,(@%s\r\n") % (channels, )
-    ser.write("READ?\r\n")
+    ser.write("CONF:TEMP TC, T,(@%s\r\n".encode('utf-8')) % (channels, )
+    ser.write("READ?\r\n".encode('utf-8'))
     InstrumentReturn = ser.readline()
     try:
         bbb = map(float, InstrumentReturn.split(","))
     except:
         bbb = [-999, -999]
-        ser.write("*RST\r\n")
+        ser.write("*RST\r\n".encode('utf-8'))
         time.sleep(1.0)
-        ser.write("*CLS\r\n")
+        ser.write("*CLS\r\n".encode('utf-8'))
         time.sleep(1.0)
     return bbb
 
