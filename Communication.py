dba2d2b9105d9ea1d051c2a98686dcc18e5b3fbb
fera0008 <felix.ragnarsson.0008@student.uu.se>
2017-05-23 12:21:19 +0200

    test

--- Communication.py
+++ Communication.py
@@ -1,12 +1,13 @@
 import serial, time
 
+    ser = serial.Serial("/dev/ttyUSB0", 57600, timeout=1, xonxoff=1)
+    ser.write("*RST\r\n")
+    ser.write("*CLS\r\n")
 
 
 
 def measure():
-    ser = serial.Serial("/dev/ttyUSB0", 57600, timeout=3, xonxoff=1)
-    ser.write("*RST\r\n")
-    ser.write("*CLS\r\n")
+
     ser.write("CONF:VOLT:DC 10,0.00001,(@101:118\r\n")
     ser.write("READ?\r\n")
     InstrumentReturn = ser.readline()
@@ -21,9 +22,6 @@ def measure():
     return bbb
 
 def temperature(channels):
-    ser = serial.Serial("/dev/ttyUSB0", 57600, timeout=3, xonxoff=1)
-    ser.write("*RST\r\n")
-    ser.write("*CLS\r\n")
     ser.write("CONF:TEMP TC, T,(@%s\r\n") % (channels, )
     ser.write("READ?\r\n")
     InstrumentReturn = ser.readline()
