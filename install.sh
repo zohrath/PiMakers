69380dd3239c72733dfd8b30e2371f3a7876f80c
nurre <nurre13@gmail.com>
2017-06-13 16:03:18 +0200

    Added script for installing program.

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+function ask_yes_or_no() {
+    read -p "$1 : "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+
+if ["no" == $(ask_yes_or_no "Have you ran 'sudo apt-get update' and 'sudo apt-get upgrade'? [Y/n]")]
+   
+then
+    echo "Do that before running the installation."
+    exit 0
+fi
+
+echo "Starting installation of program and the packages needed."
+    
+cd ~
+
+echo "Getting required packages."
+sudo pip3 install PyQt5
+sudo pip3 install matplotlib
+sudo pip3 install pymysql
+sudo pip3 install pyserial
+
+echo "mysql-server will now be installed, you will be prompted to enter a password for the root user."
+sleep 5
+sudo apt-get -y install mysql-server --fix-missing
+
+find ~ -name PiMakers.desktop | xargs -I '{}' cp {} ~/Desktop
+
